59742281db148821e4481dc1757c1f3c0ea55593
craig <kelleher.craig@gmail.com>
2022-11-15 21:52:16 +0000

    practicing with mixed lists

---
+++ challenges/lab28.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+mylist= [1,2,3,4,5, "Python"]
+
+name= input("What is your name?\n")
+name_cap = name.capitalize()
+# This is what you should see when print runs-
+# Hi <name>! Welcome to Day 2 of Python Training!
+print(f"Hi {name_cap}! Welcome to Day {mylist[1]} of {mylist[5]} Training!")
+
---
+++ no_shebang.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that\'s close enough.")
+
+
