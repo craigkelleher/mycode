0609f85855bd57e1f2f0a37aa8f180eb99c903e9
craig <kelleher.craig@gmail.com>
2022-11-16 21:37:52 +0000

    copying local files with python and making backups

---
+++ copycat01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# import additional code to complete our task
+import shutil
+import os
+
+def main():
+    # move into the working directory
+    os.chdir("/home/student/mycode/")
+
+    # copy the fileA to fileB
+    shutil.copy("5g_research/sdn_network.txt", "5g_research/sdn_network.txt.copy")
+
+    # copy the entire directoryA to directoryB
+    shutil.copytree("5g_research/", "5g_research_backup/")
+
+if __name__ == "__main__":
+    main()
